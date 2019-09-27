887329e3eb6823c71c1175e6279d8cc251a8981d
Bengi Mizrahi <bengi.mizrahi@argela.com.tr>
2019-09-27 13:25:07 +0300

    Create the add-on script file with metadata

---
+++ slideblend.py
@@ -0,0 +1,14 @@
+bl_info = {
+    "name": "Slide Blend",
+    "description": "Prepare 3D slide shows as an alternative to tools like MS Powerpoint",
+    "version": (0, 0, 1),
+    "blender": (2, 8, 0),
+    "category": "Functionality",
+    "author": "Bengi Mizrahi",
+}
+
+def register():
+    print("Hello World")
+
+def unregister():
+    print("Goodbye World")
